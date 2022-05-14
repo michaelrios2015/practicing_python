b185fdaab18e7418a27138f407f9c9c25d00d383
michaelrios2015 <michael.rios093@gmail.com>
2022-05-13 21:33:25 -0400

    a little more

--- distance.py
+++ distance.py
@@ -1,1 +1,9 @@
-speed = input("what is the speed the vehicle traveled in mph?")
+speed = int(input("what is the speed the vehicle traveled in mph? "))
+
+time = int(input("How many miles have you traveled?  "))
+
+i = 1
+
+while i <= time:
+    print(i * speed)
+    i += 1
